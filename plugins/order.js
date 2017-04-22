03a46c64bc0a1854fa4afbef89ddcc5b6b901123
Adam Levin <adamlevin@tutordelphia.com>
2017-04-22 12:02:13 -0400

    supports dynamic addresses

--- plugins/order.js
+++ plugins/order.js
@@ -1,18 +1,56 @@
 const pizzapi = require('dominos')
 
+
+module.exports = {
+  init: (controller) => {
+    controller.hears([/I/], ['direct_message', 'direct_mention'], (bot, message) => bot.createConversation(message, setUpConvo))
+
+  },
+  help: {
+    command: 'order',
+    text: `Say "I want pizza" and I'll find it.`
+  }
+}
+
+function setUpConvo(err, convo) {
+  convo.addMessage('some text', 'thread_1')
+  convo.addQuestion('What is your address?', (responseObj) => {
+    //console.log(responseObj)
+    convo.setVar("address", responseObj)
+    get_stores(convo.vars.address.text, function (stores) {
+      convo.setVar("stores", stores)
+      convo.addMessage(`Here are some nearby stores: {{#vars.stores}}\r\n{{StoreID}}: {{AddressDescription}} {{/vars.stores}}`, `list-stores`)
+      convo.gotoThread('list-stores')
+    })
+    //convo.addMessage(results, 'show_results')
+
+  }, {}, 'get_address')
+
+
+
+
+  convo.activate()
+  convo.gotoThread('get_address')
+}
+
+function get_stores(address, processResults) {
+  console.log(address)
+  pizzapi.Util.findNearbyStores(address, 'Delivery', (storeData) => {
+    stores = storeData.result.Stores.filter((store) => {
+      return (store['IsOnlineCapable'] && store['IsOnlineNow'] && store['IsOpen'])
+    })
+    console.log(stores)
+    processResults(stores)
+  })
+
+}
+
+/*
 module.exports = {
   init: (controller) => {
     controller.hears([/I/], ['direct_message', 'direct_mention'], (bot, message) => {
       bot.reply(message, `Looking for nearby stores...`)
-      pizzapi.Util.findNearbyStores('11 Times Square, New York, NY 10036', 'Delivery', (storeData) => {
-        var stores = storeData.result.Stores.filter((store) => {
-          return (store['IsOnlineCapable'] && store['IsOnlineNow'] && store['IsOpen'])
-        })
-        for (var index in stores) {
-          console.log(stores[index])
-          bot.reply(message, `${stores[index]['StoreID']} : ${stores[index]['AddressDescription']}`)
-        }
-      })
+
     })
   },
   help: {
@@ -20,3 +58,6 @@ module.exports = {
     text: `Say "I want pizza" and I'll find it.`
   }
 }
+
+
+*/
